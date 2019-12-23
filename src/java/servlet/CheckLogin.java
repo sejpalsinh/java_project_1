d8145b4cec1b2b7ee0a0a773317014d4004cdba0
sejpalsinh <55375017+sejpalsinh@users.noreply.github.com>
2019-12-23 17:54:04 +0530

    login added

---
+++ src/java/servlet/CheckLogin.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author sejpalsinh
+ */
+public class CheckLogin extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         resp.setContentType("text/html;charset=UTF-8");
+        if(StudentDAO.Student_data.check_user(req.getParameter("uname"),req.getParameter("pass")))
+            resp.sendRedirect("logon.html");
+        else
+            resp.sendRedirect("index.html");
+    }
+    
+}
